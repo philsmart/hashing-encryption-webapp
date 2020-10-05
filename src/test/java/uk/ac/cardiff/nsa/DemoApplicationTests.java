f8636a7e60c91a69b0113da281c4c4c7d61dba50
Philip Smart <philipsmart@MacBook-Pro.local>
2020-10-05 20:00:16 +0100

    First push

---
+++ src/test/java/uk/ac/cardiff/nsa/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.cardiff.nsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
